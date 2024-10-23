3860da855d755da605d3bd080962e7fe21f9dc63
James Harrison <jlharrison4706@gmail.com>
2024-10-23 18:49:04 +0100

    Implement backend API and storage of transactions using Mongo Atlas

---
+++ api/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const Transaction = require('./models/Transaction.js');
+const { default: mongoose } = require('mongoose');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.get('/api/test', (req, res) => {
+  res.json('test ok');
+});
+
+app.post('/api/transaction', async (req, res) => {
+  await mongoose.connect(process.env.MONGO_URL);
+  const {price, name, description, datetime} = req.body;
+  const transaction = await Transaction.create({price, name, description, datetime});
+  res.json(transaction);
+});
+
+app.listen(3001);
--- src/App.js
+++ src/App.js
@@ -1,16 +1,53 @@
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [name, setName] = useState();
+  const [datetime, setDatetime] = useState();
+  const[description, setDescription] = useState();
+
+  function addNewTransaction(ev) {
+    ev.preventDefault();
+    const url = process.env.REACT_APP_API_URL+'/transaction';
+    const price = name.split(' ')[0];
+
+    fetch(url, {
+      method: 'POST',
+      headers: {'Content-type':'application/json'},
+      body: JSON.stringify({
+        price,
+        name:name.substring(price.length+1),
+        description,
+        datetime
+      })
+    }).then(response => {
+      response.json().then(json => {
+        setName('');
+        setDatetime('');
+        setDescription('');
+        console.log('result', json);
+      });
+    });
+  };
+
   return (
     <main>
       <h1>$400<span>.00</span></h1>
-      <form>
+      <form onSubmit={addNewTransaction}>
         <div className="basic">
-          <input type="text" placeholder={'+200 new samsung tv'}/>
-          <input type="datetime-local"/>
+          <input type="text"
+                 value={name}
+                 onChange={ev => setName(ev.target.value)}
+                 placeholder={'+200 new samsung tv'}/>
+          <input type="datetime-local"
+                 value={datetime}
+                 onChange={ev => setDatetime(ev.target.value)}/>
         </div>
         <div className="description">
-          <input type="text" placeholder={'description'}/>
+          <input type="text" 
+                 value={description}
+                 onChange={ev => setDescription(ev.target.value)}
+                 placeholder={'description'}/>
         </div>
         <button type="submit">Add new transaction</button>
       </form>
